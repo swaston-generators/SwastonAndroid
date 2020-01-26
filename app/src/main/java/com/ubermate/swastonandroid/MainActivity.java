56be55b1b68ba3622fb661b04602e6de64201022
Wirtos <Wirtos.new@gmail.com>
2020-01-26 11:31:35 +0200

    fancy animations, all the textView properties are inside xml now

--- app/src/main/java/com/ubermate/swastonandroid/MainActivity.java
+++ app/src/main/java/com/ubermate/swastonandroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -33,14 +34,44 @@ public class MainActivity extends Activity {
 
     }
 
+    private void resetPosition() {
+        textView.setRotation(0);
+        textView.setRotationX(0);
+        textView.setRotationY(0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         textView = findViewById(R.id.output);
-        textView.setTypeface(Typeface.MONOSPACE);
+
+        textView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                resetPosition();
+                textView.animate().rotationBy(-75).setDuration(1000).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.animate().rotationBy((360 * 8) + 75).setDuration(1000);
+
+                    }
+                });
+                return true;
+            }
+        });
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetPosition();
+                textView.animate().rotationYBy(-360).rotationBy(360).setDuration(1000);
+            }
+        });
+
+
         EditText editText = findViewById(R.id.input);
+
 
         FloatingActionButton copyButton = findViewById(R.id.copy);
         copyButton.setOnClickListener(new View.OnClickListener() {
@@ -52,9 +83,10 @@ public class MainActivity extends Activity {
         editText.addTextChangedListener(new TextWatcher() {
 
             public void afterTextChanged(Editable s) {
-                textView.setAlpha(0f);
-                textView.setRotation(180f);
-                textView.animate().alpha(1).rotationBy(180f).setDuration(450);
+                resetPosition();
+                textView.setAlpha(0);
+                textView.setRotation(180);
+                textView.animate().alpha(1).rotationBy(180).setDuration(450);
 
                 textView.setText(SwastonGenerator.FromString(s.toString()));
 
